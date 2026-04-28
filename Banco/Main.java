bdbc7bf330cadbb42c45f8492e99e76645d22130
Leticia Pessôa <lpessoamachado@gmail.com>
2026-04-27 23:16:54 -0300

    Correção da Main

--- Banco/Main.java
+++ Banco/Main.java
@@ -2,15 +2,33 @@ public class Main {
     public static void main(String[] args) {
         Banco meuBanco = new Banco("Banquinho");
 
-        //acrescentar 5 funcionarios e 2 gerentes a um banco
+        //Adição de funcionarios
+        meuBanco.adicionarFuncionario("482.391.657-09", "Selton Melo", "M2847", false);
+        meuBanco.adicionarFuncionario("295.618.473-81", "Fernanda Montenegro", "M7062", false); // Gerente
+        meuBanco.adicionarFuncionario("864.237.915-36", "Wagner Moura", "M4381", false);
+        meuBanco.adicionarFuncionario("319.284.756-03", "Rodrigo Santoro", "M3516", false);
+        meuBanco.adicionarFuncionario("628.413.597-28", "Alessandra Negrini", "M6749", false);
 
-        meuBanco.adicionarFuncionario("482.391.657-09", "Fulano", "M2847", false);
-        meuBanco.adicionarFuncionario("731.045.892-14", "Sicrano", "M1593", true);
-        meuBanco.adicionarFuncionario("295.618.473-81", "Beltrano", "M7062", false); // Gerente
-        meuBanco.adicionarFuncionario("864.237.915-36", "Fulana", "M4381", false);
-        meuBanco.adicionarFuncionario("547.923.186-52", "Sicrana", "M9274", true);
+        //Adição de Gerentes
+        meuBanco.adicionarFuncionario("547.923.186-52", "Débora Falabella", "M9274", true);
+        meuBanco.adicionarFuncionario("731.045.892-14", "Lazaro Ramos", "M1593", true);
 
-        //promover 1 funcionario e 1 gerente
+        Gerente gerente1 = meuBanco.localizarGerente("M9274");
+        Gerente gerente2 = meuBanco.localizarGerente("M1593");
+
+        //Distribuição de funcionários entre gerentes
+        if (gerente1 != null){
+            gerente1.adicionarSubordinado(meuBanco.localizarFuncionario("M2847"));
+            gerente1.adicionarSubordinado(meuBanco.localizarFuncionario("M7062"));
+            gerente1.adicionarSubordinado(meuBanco.localizarFuncionario("M4381"));
+        }
+
+        if(gerente2 != null){
+            gerente2.adicionarSubordinado(meuBanco.localizarFuncionario("M3516"));
+            gerente2.adicionarSubordinado(meuBanco.localizarFuncionario("M6749"));
+        }
+
+        //Promover 1 funcionario e 1 gerente
         meuBanco.promoverFuncionario(meuBanco.localizarFuncionario("M2847"));
         meuBanco.promoverFuncionario(meuBanco.localizarFuncionario("M9274"));
     }
